91bd40b9522bdab6d298c9c9828d424c6f7578fc
Dharmesh Kashyap <dharmeshkashyap143@gmail.com>
2025-02-06 17:20:18 +0530

    changes

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,16 +5,16 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <a href="#" target="_blank" rel="noopener noreferrer">
+        {/* <a href="#" target="_blank" rel="noopener noreferrer">
           AML Logix
-        </a>
-        <span className="ms-1">&copy; 2025 creativeLabs.</span>
+        </a> */}
+        {/* <span className="ms-1">&copy; 2025 creativeLabs.</span> */}
       </div>
       <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="#" target="_blank" rel="">
+        {/* <span className="me-1">Powered by</span> */}
+        {/* <a href="#" target="_blank" rel="">
           AML Logix React Admin Panel
-        </a>
+        </a> */}
       </div>
     </CFooter>
   )
